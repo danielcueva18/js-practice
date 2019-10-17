6281a59473becb5cda0493e063314b66e08896a7
Daniel Cueva <cuevadaniel1@gmail.com>
2019-10-16 20:28:29 -0700

    successful kelvin to farenheit project

---
+++ projects/kelvin-farenheit.js
@@ -0,0 +1,13 @@
+// Temperature in Kelvin
+var kelvinTemp = 80
+// Celsius temp is 273.15 less than Kelvin
+var celsiusTemp = kelvinTemp - 273.15
+// equation to get farenheit
+var fahrenheitTemp = celsiusTemp * (9/5) + 32
+// adding Math.floor equation to floor the number which removes the decimals and assigns that the the farenheit temp variable
+fahrenheitTemp = Math.floor(fahrenheitTemp)
+// Concatanates a string and the farenheit temperature 
+console.log('The temperature is ' + (fahrenheitTemp + ' ' + 'degrees fahrenheit.'))
+console.log('The temp in Kelvin is ' + kelvinTemp)
+celsiusTemp = Math.floor(celsiusTemp)
+console.log('The temp in in Celsius is ' + celsiusTemp)
