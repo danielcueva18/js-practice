002226010c1cd6d8e82678f3fbe1fc681ac025b7
Daniel Cueva <cuevadaniel1@gmail.com>
2019-10-22 23:59:06 -0700

    arrays practice var thing [0, 'hi]

---
+++ practice/arrays.js
@@ -0,0 +1,20 @@
+// Challenge
+// Add value to the end of the array
+// turn 'Giants' into a different team
+// access the 'Dodgers' value and save it to variable myFavoriteTeam
+// access the length of the array
+
+var baseballTeam = ['Dodgers', 'Giants', 'Mets', 'Yankees', 'Astros'];
+
+console.log(baseballTeam.length)
+
+baseballTeam[5] = 'Cardinals'
+console.log(baseballTeam)
+
+baseballTeam[1] = 'Red Soxs'
+console.log(baseballTeam)
+
+var myFavoriteTeam = baseballTeam[0]
+console.log(myFavoriteTeam)
+
+console.log(baseballTeam.length)
