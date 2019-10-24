30ec392ef610bbebe78673ca3d690594c2881696
Daniel Cueva <cuevadaniel1@gmail.com>
2019-10-23 22:28:16 -0700

    Insult generator project

---
+++ projects/random-insult-generator.js
@@ -0,0 +1,13 @@
+var randomBodyParts = ['forehead', 'penis', 'vagina', 'butthole'];
+var randomAdjectives = ['gross', 'smelly', 'limpy', 'pale'];
+var randomWords = ['pikachu', 'pebble', 'table', 'yogurt', 'naan'];
+
+var randomBodyPart = randomBodyParts[Math.floor(Math.random() * 4)];
+var randomAdjective = randomAdjectives[Math.floor(Math.random() * 4)];
+var randomWord = randomWords[Math.floor(Math.random() * 5)];
+
+console.log('Your ' + randomBodyPart + ' ' + 'is like a ' + randomAdjective + ' ' + randomWord)
+
+
+
+
